e7f961432d1c6722ee00b1955e3a5720e64d5888
Sucharita Nandy <sucharitan654@gmail.com>
2025-06-06 07:55:17 +0530

    resolved conflicts

--- qb/src/components/Navbar.js
+++ qb/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -13,13 +14,7 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Dashboard</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/subject/1/add-quiz/2">Add Quiz</Link>
-            </li>
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/subject/1/add-chapter">Add Chapter</Link>
             </li>
