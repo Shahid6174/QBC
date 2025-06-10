965e396acd6fe37881d6b688477e76dbf4b36775
= <=>
2025-06-10 09:20:09 +0530

    Fix: #20 Adds profile pictures and allow user to update their profile information with better ui

--- website/__init__.py
+++ website/__init__.py
@@ -76,8 +76,9 @@ def create_app():
 
     from .views import views
     from .auth import auth
-    
-    app.register_blueprint(views, url_prefix='/')
+   
+    app.register_blueprint(views, url_prefix="/")
+   
     app.register_blueprint(auth, url_prefix='/')
 
     create_database(app)
